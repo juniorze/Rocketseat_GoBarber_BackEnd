25bd66f5dc82636f0313c142f84d3a787b1427e6
juniorze <junior.ze.jw@gmail.com>
2019-08-28 18:04:58 -0300

    fix: include nodemon.json and index.js

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-   const user = await User.create({
-      name: 'Junior Silva',
-      email: 'junior.ze.jw@gmail.com',
-      password_hash: '324242343233',
-   });
+routes.post('/users', UserController.store);
 
-   return res.json(user);
-});
 export default routes;
